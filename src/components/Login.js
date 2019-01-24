e2cf6957dafc40bb7362988cbde2b85318f2d772
Nghia. Nguyen Van (2) <nghinv2@vng.com.vn>
2019-01-24 15:23:08 +0700

    form validate

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import { Form, Input, Button, Icon } from 'antd';
+import { Link } from 'react-router-dom';
 
 import textLogo from '../img/icon_text.png';
 
 class Login extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, value) => {
+            if(!err) {
+                console.log('Receive values of form: ', value);
+            }
+        });
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;
+
         return (
             <div className='login-container'>
                 <div className='login-area'>
@@ -13,19 +25,44 @@ class Login extends Component {
                     </div>
                     <div className='login-main'>
                         <h2 className='form-title'>Login to NT Chat</h2>
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <Form.Item>
-                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                                {
+                                    getFieldDecorator('username', {
+                                        rules: 
+                                        [{ 
+                                            required: true, message: 'Please input your username!' } ,
+                                        {
+                                            min: 6, message: 'Username has at least 6 characters!'
+                                        }],
+                                    })(
+                                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                                    )
+                                }
                             </Form.Item>
                             <Form.Item>
-                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                {
+                                    getFieldDecorator('password', {
+                                        rules: [
+                                            { 
+                                                required: true, message: 'Please input password!' },
+                                            {
+                                                min: 6, message: 'Password has at least 6 characters!'
+                                            },
+                                            {
+                                                validator: this.validateToNextPassword
+                                            }],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                    )
+                                }
                             </Form.Item>
                             <Form.Item>
                                 <Button type="primary" htmlType="submit" className="login-form-button">
                                     Log in
                                 </Button>
                                 <span className='login-tips'>
-                                    Chưa có tài khoản? <a href='#'>Đăng ký</a>
+                                    Chưa có tài khoản? <Link to='/signup'>Đăng ký</Link>
                                 </span>
                             </Form.Item>
                         </Form>
@@ -36,4 +73,4 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default Form.create({name: 'login_form'})(Login);
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
-
-import { Form, Input, Button, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+import { Form, Input, Button, Icon, notification } from 'antd';
 
 import textLogo from '../img/icon_text.png';
 
 class Signup extends Component {
+    state = {
+        confirmDirty: false
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        notification['success']({
+            message: 'Success',
+            description: 'Đăng ký tài khoản thành công',
+            duration: 2
+        });
+        this.props.form.validateFields((err, value) => {
+            if(!err) {
+                console.log('Receive values of form: ', value);
+            }
+        });
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({confirmDirty: this.state.confirmDirty || !! value});
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if(value && value !== form.getFieldValue('password')) {
+            callback('Two password that you enter is inconsistent!');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if(value && this.state) {
+            form.validateFields(['confirm'], {force: true});
+        }
+        callback();
+    }
+
     render() {
+        const { getFieldDecorator } = this.props.form;
+
         return (
             <div className='login-container'>
                 <div className='login-area'>
@@ -14,25 +56,75 @@ class Signup extends Component {
                     </div>
                     <div className='login-main'>
                         <h2 className='form-title'>Sign up an account</h2>
-                        <Form>
+                        <Form onSubmit={this.handleSubmit}>
                             <Form.Item>
-                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                                {
+                                    getFieldDecorator('username', {
+                                        rules: 
+                                        [{ 
+                                            required: true, message: 'Please input your username!' } ,
+                                        {
+                                            min: 6, message: 'Username has at least 6 characters!'
+                                        }],
+                                    })(
+                                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                                    )
+                                }
                             </Form.Item>
                             <Form.Item>
-                                <Input prefix={<Icon type="smile" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Display name" />
+                                {
+                                    getFieldDecorator('display_name', {
+                                        rules: 
+                                        [{ 
+                                            required: true, message: 'Please input your display name!' } ,
+                                        {
+                                            min: 6, message: 'Display name has at least 6 characters!'
+                                        }],
+                                    })(
+                                        <Input prefix={<Icon type="smile" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Display name" />
+                                    )
+                                }
                             </Form.Item>
                             <Form.Item>
-                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                {
+                                    getFieldDecorator('password', {
+                                        rules: [
+                                            { 
+                                                required: true, message: 'Please input your password!' },
+                                            {
+                                                min: 6, message: 'Password has at least 6 characters!'
+                                            },
+                                            {
+                                                validator: this.validateToNextPassword
+                                            }],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                                    )
+                                }
                             </Form.Item>
                             <Form.Item>
-                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Re-Password" />
+                                {
+                                    getFieldDecorator('confirm', {
+                                        rules: [
+                                            { 
+                                                required: true, message: 'Please confirm your password!' },
+                                            {
+                                                min: 6, message: 'Password has at least 6 characters!'
+                                            }, 
+                                            {
+                                                validator: this.compareToFirstPassword
+                                            }],
+                                    })(
+                                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Re-Password" />
+                                    )
+                                }
                             </Form.Item>
                             <Form.Item>
                                 <Button type="primary" htmlType="submit" className="login-form-button">
                                     Sign up
                                 </Button>
                                 <span className='login-tips'>
-                                    Đã có tài khoản? <a href='#'>Đăng nhập</a>
+                                    Đã có tài khoản? <Link to='/login'>Đăng nhập</Link>
                                 </span>
                             </Form.Item>
                         </Form>
@@ -43,4 +135,4 @@ class Signup extends Component {
     }
 }
 
-export default Signup;
+export default Form.create({name: 'signup_form'})(Signup);
